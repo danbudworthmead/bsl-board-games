98c5f1d74d7a4f2ede2c18dcb30bc74e0f23eebb
danbudworthmead <danbudworthmead@gmail.com>
2025-03-17 20:58:40 +0000

    Fix website page not showing content
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/danbudworthmead/bsl-board-games?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './main.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
