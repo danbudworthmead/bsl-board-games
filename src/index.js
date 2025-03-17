ea6c612b1c74e49e5d8ad4b3cfba19313f43d942
danbudworthmead <danbudworthmead@gmail.com>
2025-03-17 21:14:32 +0000

    trying to get the damn website to load

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './main.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
